0f12176ebbfd641cfe30e714d69c258e33e77426
The Verse <floatbeta@gmail.com>
2024-06-26 22:18:09 -0500

    Update app.go

--- app/app.go
+++ app/app.go
@@ -1,14 +1,14 @@
 package app
 
 import (
-	"pandora-pay/address_balance_decryptor"
-	"pandora-pay/blockchain"
-	"pandora-pay/blockchain/forging"
-	"pandora-pay/gui"
-	"pandora-pay/mempool"
-	"pandora-pay/settings"
-	"pandora-pay/store"
-	"pandora-pay/wallet"
+	"mc/address_balance_decryptor"
+	"mc/blockchain"
+	"mc/blockchain/forging"
+	"mc/gui"
+	"mc/mempool"
+	"mc/settings"
+	"mc/store"
+	"mc/wallet"
 )
 
 var (
